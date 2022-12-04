8ca31ace482e91302a7284988cdd5a488e9faa66
Alex Wilcox <alex.wilcox@cloudsecure.ltd>
2022-12-04 19:46:49 +0000

    New stats card content

--- src/views/cards/statsCard.js
+++ src/views/cards/statsCard.js
@@ -10,6 +10,7 @@ const StatsCard = () => {
                         <li className="list-group-item">
                             <h3>Main Hall - Capacity 300 people</h3>
                             <ul>
+                                <li>Social Distancing Capacity - 50 people</li>
                                 <li>Stage (with Spot and Stage Lighting)</li>
                                 <li>PA System</li>
                                 <li>Loop Hearing System</li>
@@ -22,6 +23,7 @@ const StatsCard = () => {
                         <li className="list-group-item">
                             <h3>Club Room - Capacity 40 people</h3>
                             <ul>
+                                <li>Social Distancing Capacity - 13 people</li>
                                 <li>Use of main kitchen</li>
                                 <li>Use of tables and chairs</li>
                                 <li>3K Lumen Projector available to hire @ £5 per hour</li>
@@ -31,6 +33,7 @@ const StatsCard = () => {
                         <li className="list-group-item">
                             <h3>Committee Room - Capacity 30 people</h3>
                             <ul>
+                                <li>Social Distancing Capacity - 9 people</li>
                                 <li>Use of main kitchen</li>
                                 <li>Use of tables and chairs</li>
                                 <li>Guest WiFi (free)</li>
@@ -39,6 +42,7 @@ const StatsCard = () => {
                         <li className="list-group-item">
                             <h3>Annex Room - Capacity 30 people</h3>
                             <ul>
+                                <li>Social Distancing Capacity - 8 people</li>
                                 <li>Small kitchen for refreshments</li>
                                 <li>Use of tables and chairs</li>
                                 <li>Guest WiFi (free)</li>
