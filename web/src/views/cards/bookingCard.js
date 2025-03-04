517be150dd5a96eb81826142c90118631e7e3553
Alex Wilcox <alex.wilcox@cloudsecure.ltd>
2025-03-04 23:14:43 +0000

    Latest Updates

--- web/src/views/cards/bookingCard.js
+++ web/src/views/cards/bookingCard.js
@@ -8,8 +8,8 @@ const BookingCard = () => {
                 <p>Please read <a target="_blank" rel="noopener noreferrer" href={`${process.env.PUBLIC_URL}/Documents/Wvh_Hire_Conditions.pdf`}>Our Hire Conditions</a></p>
                 <div className="form-link-container">
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Booking_Form_single_Event.pdf`} target="_blank" rel="noopener noreferrer" id="booking-btn">Single Event Bookings</a></li>
-                        <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Regular_Booking_Form.pdf`} target="_blank" rel="noopener noreferrer" id="booking-btn">Reoccuring Events Bookings</a></li>
+                        <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/booking`} id="booking-btn">Single Event Bookings</a></li>
+                        <li className="list-group-item"><a href={`https://v2.hallmaster.co.uk/Account/Login?hid=11938`} target="_blank" rel="noopener noreferrer" id="booking-btn">Regular Events Bookings</a></li>
                     </ul>
                 </div>
             </div>
